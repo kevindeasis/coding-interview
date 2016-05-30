172cefc80b7cab6cb0892a41a0dd3e6769c8f2df
Kevin De Asis <deasis@ualberta.ca>
2016-05-30 16:41:29 -0600

    chore(ui-layout): figured out what ui layout will look like for the home page

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('hello world reload')
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class Sidebar extends React.Component {
+    render() {
+        return (
+            <div>
+                ei
+            </div>
+        )
+    }
+}
+ReactDom.render(<Sidebar />, document.getElementById('slide-out'))
